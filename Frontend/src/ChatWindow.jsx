65c861e6a00db7bd1acccd4c9bad4bd37f97ee71
sambit578 <sambitkumarbal24@gmail.com>
2026-08-05 18:12:22 +0530

    Styling Chat Window

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,9 +1,27 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow(){
     return(
-        <div>
-            ChatWindow
+        <div className="chatWindow">
+            <div className="navbar">
+                <span>Nexus AI <i class="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv">
+                    <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            <Chat></Chat>
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask your Query">
+
+                    </input>
+                    <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+                </div>
+                <p className="info">
+                    Build by NEXUS TEAM
+                </p>
+            </div>
         </div>
     )
 }
