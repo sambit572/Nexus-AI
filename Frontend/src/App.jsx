7df30ecbe36f2065e43429bbcda2462eca974c54
sambit578 <sambitkumarbal24@gmail.com>
2026-08-06 18:01:39 +0530

    Fetching Data from Chat

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,9 +2,18 @@ import './App.css';
 import Sidebar from "./Sidebar.jsx";
 import ChatWindow from "./ChatWindow.jsx";
 import { MyContext } from "./MyContext.jsx";
+import { useState } from 'react';
+import {v1 as uuidv1} from "uuid";
 
 function App() {
-  const providerValue={};
+  const [prompt,setPrompt]=useState("");
+  const [reply,setReply]=useState(null);
+  const [currThreadId,setCurrThreadId]=useState(uuidv1());
+  const providerValue={
+    prompt,setPrompt,
+    reply,setReply,
+    currThreadId,setCurrThreadId
+  };
   return (
     <div className="app">
       <MyContext.Provider value={providerValue}>
--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,7 +1,29 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useContext } from "react";
 
 function ChatWindow(){
+
+    const {prompt,setPrompt,reply,setReply,currThreadId}=useContext(MyContext);
+    const getReply=async()=>{
+        const options={
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:{
+                message:prompt,
+                threadId:currThreadId
+            }
+        };
+        try {
+            await fetch("http://localhost:8080/api/chat");
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return(
         <div className="chatWindow">
             <div className="navbar">
@@ -13,10 +35,13 @@ function ChatWindow(){
             <Chat></Chat>
             <div className="chatInput">
                 <div className="inputBox">
-                    <input placeholder="Ask your Query">
-
+                    <input placeholder="Ask your Query"
+                    value={prompt}
+                    onChange={(e)=>setPrompt(e.target.value)}
+                    >
+                        
                     </input>
-                    <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+                    <div id="submit" onClick={getReply}><i class="fa-solid fa-paper-plane"></i></div>
                 </div>
                 <p className="info">
                     Build by NEXUS TEAM
