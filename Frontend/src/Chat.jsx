34e6ce5dab4185bca04894d089ec0fcc74c98ba3
sambit578 <sambitkumarbal24@gmail.com>
2026-08-08 17:55:49 +0530

    Formatting Nexus Reply

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,6 +1,9 @@
 import "./Chat.css";
 import { useContext } from "react";
 import { MyContext } from "./MyContext.jsx";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 
 function Chat(){
     const {preChats,newChats} = useContext(MyContext);
@@ -8,8 +11,18 @@ function Chat(){
         <>
             {newChats && <h1>Begin a New Journey</h1>}
             <div className="chats">
-                <div className="userDiv"></div>
-                <div className="nexusDiv"></div>
+                {
+                    preChats?.map((chat,idx)=>
+                        <div className={chat.role==="user"?"userDiv":"nexusDiv"} key={idx}>
+                            {
+                                chat.role === "user" ? 
+                                <p className="userMessage">{chat.content}</p> : 
+                                <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{chat.content}</ReactMarkdown>
+                            }
+                        </div>
+                    )
+                }
+                
             </div>
         </>
     )
--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,12 +1,12 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
 import { MyContext } from "./MyContext.jsx";
-import { useContext,useState} from "react";
+import { useContext,useState,useEffect} from "react";
 import {ScaleLoader} from "react-spinners";
 
 function ChatWindow(){
 
-    const {prompt,setPrompt,reply,setReply,currThreadId}=useContext(MyContext);
+    const {prompt,setPrompt,reply,setReply,currThreadId,preChats,setPreChats}=useContext(MyContext);
     const [loading,setLoading]=useState(false);
     const getReply=async()=>{
         setLoading(true);
@@ -31,6 +31,21 @@ function ChatWindow(){
         }
         setLoading(false);
     }
+
+    useEffect(()=>{
+        if(prompt && reply){
+            setPreChats(preChats => (
+                [...preChats,{
+                    role:"user",
+                    content:prompt
+                },{
+                    role:"assistant",
+                    content:reply
+                }]
+            ));
+        }
+        setPrompt("");
+    },[reply]);
 
     return(
         <div className="chatWindow">
